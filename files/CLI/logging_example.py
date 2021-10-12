a86528af34f9797b6e42cbdfd9ef5dbf77487be9
Mikhal Zelenyy <mihail.zelenyy@phystech.edu>
2021-10-12 17:07:53 +0300

    dev-lecture argv code example

---
+++ files/CLI/logging_example.py
@@ -0,0 +1,15 @@
+import logging
+import somelib
+
+def main():
+
+    logging.basicConfig(filename='myapp.log',
+                        level=logging.INFO,
+                        # format="%(asctime)-15s %(levelname)s %(message)s"
+                        )
+    logging.info('Started')
+    somelib.call()
+    logging.info('Finished')
+
+if __name__ == '__main__':
+    main()
---
+++ files/CLI/somelib.py
@@ -0,0 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def call():
+    logger.error("This is main error of your life.")
